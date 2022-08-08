06f8b6aa8f6a671160788467810e6fbd0494e4f8
rowlinson <johk2293@gmail.com>
2022-08-08 10:15:02 +0400

    Make django project and app, update setup scripts

--- init.sh
+++ init.sh
@@ -5,3 +5,7 @@ sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/etc/gunicorn.conf.py /etc/gunicorn.d/test
 sudo gunicorn -c /etc/gunicorn.d/test hello
+
+
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf.py /etc/gunicorn.d/ask
+sudo gunicorn -c /etc/gunicorn.d/ask ask.wsgi
