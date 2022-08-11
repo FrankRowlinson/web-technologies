652510b6808697a5161be0d010e63688b341b799
rowlinson <johk2293@gmail.com>
2022-08-11 16:02:49 +0400

    More bug fixes

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -14,14 +14,16 @@ class Question(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     added_at = models.DateField(auto_now_add=True)
-    rating = models.IntegerField()
+    rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    likes = models.ManyToManyField(User)
+    likes = models.ManyToManyField(User, related_name="question_like_user")
+
+    objects = QuestionManager()
 
 
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateField(auto_now_add=True)
-    question = models.OneToOneField(Question)
+    question = models.OneToOneField(Question, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
