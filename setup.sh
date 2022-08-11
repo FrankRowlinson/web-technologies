cd6bcfa354bae31590b758faf133fa7362c9cade
rowlinson <johk2293@gmail.com>
2022-08-11 15:31:41 +0400

    Minor bug fixes

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 sudo apt update
 sudo apt install -y python3.5
+sudo apt install -y python3.5-dev
 sudo rm /usr/bin/python3
 sudo ln -s /usr/bin/python3.5 /usr/bin/python3
 sudo pip3 install gunicorn
 sudo pip3 install django==2.1
+sudo pip3 install mysqlclient==1.3.7
 sudo apt install mysql-server-5.6
