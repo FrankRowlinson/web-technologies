06280e8bfcf88b2d55f57c8f7cbe4fa80353b848
rowlinson <johk2293@gmail.com>
2022-08-11 15:09:06 +0400

    Database migration, new models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.objects.order_by('-added_at')
+
+    def popular(self):
+        return self.objects.order_by('-rating')
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    rating = models.IntegerField()
+    author = models.ForeignKey(User)
+    likes = models.ManyToManyField(User)
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    question = models.OneToOneField(Question)
+    author = models.ForeignKey(User)
+
---
+++ db_init.sh
@@ -0,0 +1,9 @@
+sudo /etc/init.d/mysql start
+
+mysql -uroot -e "CREATE DATABASE web;"
+mysql -uroot -e "CREATE USER 'box'@'localhost' INDENTIFIED BY '1234';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON stepic_web.* TO 'box'@'localhost' WITH GRANT OPTION;"
+
+cd ~/web/ask
+python3 manage.py makemigrations
+python3 manage.py migrate
