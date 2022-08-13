caffaff35260a8741d7e26d08ce14e1e7bdd05a7
rowlinson <johk2293@gmail.com>
2022-08-13 10:07:36 +0400

    Return correct models.py version

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -16,8 +16,8 @@ class Question(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
-    # author = models.ForeignKey(User, on_delete=models.CASCADE)
-    # likes = models.ManyToManyField(User, related_name="question_like_user")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name="question_like_user")
 
     objects = QuestionManager()
 
@@ -29,6 +29,5 @@ class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    # author = models.ForeignKey(User, on_delete=models.CASCADE)
-    author = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
